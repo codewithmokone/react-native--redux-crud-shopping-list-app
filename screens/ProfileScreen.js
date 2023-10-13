181477bdbc8fc306a1d7e3c332bcaa782aaf58f2
codewithmokone <codewithmokone@gmail.com>
2023-10-13 08:44:14 +0200

    updated

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ProfileScreen() {
   return (
     <View style={{flex:1, justifyContent:'center', alignItems: 'center'}}>
-      <Text>Under Construction!</Text>
+      <Text>Page 404!</Text>
       <Image style={{width: 100, height: 100, marginTop: 20}} source={require('../assets/images/construction.png')} />
     </View>
   )
